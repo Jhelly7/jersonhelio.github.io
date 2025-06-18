206f5e61b4006035dd9bf12f242de5db13ee35fd
Jhelly7 <gersonhelio07@gmail.com>
2025-06-18 03:32:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll suave ao clicar nos links do menu
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = this.getAttribute('href').substring(1);
+      document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
